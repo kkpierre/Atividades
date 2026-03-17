d5bf2654e1ccf91a3d55521893f52d5da68a24f7
Kauã <kauapsmarques@gmail.com>
2026-03-16 21:49:33 -0300

    alteração de nome das pastas

--- Celsius para Fahrenheit/info4.php
+++
@@ -1,9 +0,0 @@
-<?php
-$celsius = $_POST["celsius"];
-$fahrenheit = ($celsius * 9 / 5 + 32);
-
-
-echo "Temperatura em Celcius: ".$celsius."<br>";
-echo "Temperatura em Fahrenheit: ".$fahrenheit;
-
-?>
--- Dobro de um Número/info5.php
+++
@@ -1,9 +0,0 @@
-<?php
-$num1 = $_POST["num1"];
-$dobro = ($num1 * 2);
-
-
-echo "Primeiro Valor: ".$num1."<br>";
-echo "Dobro do Valor: ".$dobro."<br>";
-
-?>
